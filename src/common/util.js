0ea17746b1256e3eb9caf01575679c8d5f8a9975
bruce <brucechen.ca@outlook.com>
2024-12-21 11:23:27 +0800

    Add a utility function topmost to capture pop-up dialogs, simplifying the process of locating elements within the dialog

--- src/common/util.js
+++ src/common/util.js
@@ -24,10 +24,16 @@ export async function query(page, selector) {
 }
 
 export async function click(page, selector, waitForNavigation) {
-  // wait for the element being ready
-  await page.waitForSelector(`${selector}`);
+  let el;
+  if (typeof selector === 'string' || selector instanceof String) {
+    // wait for the element being ready
+    await page.waitForSelector(`${selector}`);
 
-  const [el] = await page.$$(`${selector}`);
+    [el] = await page.$$(`${selector}`);
+  } else {
+    el = page;
+    selector = waitForNavigation;
+  }
 
   if (!el)
     throw new Error(
@@ -54,3 +60,74 @@ export async function input(page, selector, content) {
   await page.focus(selector);
   await page.keyboard.type(content);
 }
+
+export async function login({
+  page,
+  username,
+  password,
+  captcha,
+  selUser = 'input[placeholder="请输入账号"]',
+  selPass = 'input[placeholder="请输入密码"]',
+  selCapt = 'input[placeholder="请输入验证码"]',
+  selLogin = `xpath/.//button[@type='button']/span/span[text()='登 录']`,
+} = {}) {
+  // login in first
+  await input(page, selUser, username).catch((e) => {
+    throw new Error(`failed to type into element '${selUser}'`);
+  });
+
+  await input(page, selPass, password).catch((e) => {
+    throw new Error(`failed to type into element '${selPass}'`);
+  });
+
+  await input(page, selCapt, captcha).catch((e) => {
+    throw new Error(`failed to type into element '${selCapt}'`);
+  });
+
+  await click(page, selLogin, true).catch((e) => {
+    throw new Error(`failed to click on button '${selLogin}'`);
+  });
+}
+
+export async function topmost(page, selector) {
+  const els = await query(page, selector);
+  const zindexes = await Promise.all(
+    els.map(async (el) => {
+      // getProperty sees an error:
+      //   !Protocol error (DOM.describeNode): Object id doesn't reference a Node
+      // let style = await el.getProperty('style');
+      // style = await style.jsonValue();
+
+      // element.style only returns the attribute name, not good enough
+      // const style = await el.evaluate((element) => element.style);
+      const style = await el.evaluate((element) =>
+        element.getAttribute('style')
+      );
+
+      const attrs = style.split(';').map((attr) => {
+        let [key, value] = attr.split(':');
+        key = key?.trim();
+        value = value?.trim();
+        return { key, value };
+      });
+
+      let zindex = attrs.findLast((attr) => attr.key === 'z-index')?.value;
+      if (zindex !== undefined) zindex = parseInt(zindex);
+      return { el, zindex };
+    })
+  );
+
+  const top = zindexes
+    .filter(({ zindex }) => zindex !== undefined)
+    .reduce((p, v) => (p.zindex > v.zindex ? p : v));
+  return top.el;
+}
+
+export default {
+  page: {
+    click,
+    input,
+    query,
+    login,
+  },
+};
